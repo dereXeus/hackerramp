3b2ac68bb5c127724fb72367e64e0e01829ee7d4
amberm291 <m.amber@iitg.ernet.in>
2016-04-17 01:39:39 +0530

    Debugging

--- ParseStarterProject/src/main/java/com/parse/starter/ui/activity/Login.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ui/activity/Login.java
@@ -3,6 +3,8 @@ package com.parse.starter.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,7 +21,7 @@ import com.parse.starter.util.CurrentUser;
 import java.util.List;
 
 
-public class Login extends Activity {
+public class Login extends ActionBarActivity {
 
     private static final String TAG = "LoginActivity";
 
--- ParseStarterProject/src/main/java/com/parse/starter/ui/activity/SearchActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ui/activity/SearchActivity.java
@@ -2,23 +2,82 @@ package com.parse.starter.ui.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 import com.parse.starter.R;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.support.v7.app.ActionBarDrawerToggle;
 
 
-public class SearchActivity extends Activity {
+public class SearchActivity extends ActionBarActivity {
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
+
+    private void addDrawerItems() {
+        String[] optionsArray = { "Home","Wallet","Order Status" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, optionsArray);
+        mDrawerList.setAdapter(mAdapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SearchActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Options");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerList = (ListView)findViewById(R.id.nav_list);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+        addDrawerItems();
+        setupDrawer();
+
         Button searchButton = (Button)  findViewById(R.id.searchButton);
         final EditText searchTerm = (EditText) findViewById(R.id.searchBar);
         searchButton.setOnClickListener(
@@ -27,7 +86,7 @@ public class SearchActivity extends Activity {
                     public void onClick(View view) {
                         String query = searchTerm.getText().toString();
                         if(TextUtils.isEmpty(query)){
-                            Toast.makeText(SearchActivity.this, "Please enter a term in sarch bar",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SearchActivity.this, "Please enter a term in search bar",Toast.LENGTH_SHORT).show();
                             return;
                         }
                         Intent searchIntent = new Intent(SearchActivity.this,SearchListActivity.class);
@@ -37,7 +96,17 @@ public class SearchActivity extends Activity {
                 });
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -55,6 +124,10 @@ public class SearchActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
